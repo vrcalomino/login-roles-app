8d4a3cac76d64db1683efa1eec18cbb623a68e91
vrcalomino <vrcalomino@gmail.com>
2023-11-22 17:42:44 -0300

    feat: added method for persistence controller

--- src/main/java/com/mycompany/login/persistencia/ControladoraPersistencia.java
+++ src/main/java/com/mycompany/login/persistencia/ControladoraPersistencia.java
@@ -1,7 +1,10 @@
 package com.mycompany.login.persistencia;
 
 import com.mycompany.login.logica.Usuario;
+import com.mycompany.login.persistencia.exceptions.NonexistentEntityException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ControladoraPersistencia {
     
@@ -13,5 +16,25 @@ public class ControladoraPersistencia {
 
     public List<Usuario> traerUsuarios() {
         return usuarioJpa.findUsuarioEntities();
+    }
+
+    public void editarUsuario(Usuario usuario) {
+        try {
+            usuarioJpa.edit(usuario);
+        } catch (Exception ex) {
+            Logger.getLogger(ControladoraPersistencia.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public Usuario traerUsuario(int id) {
+        return usuarioJpa.findUsuario(id);
+    }
+
+    public void eliminarUsuario(int id) {
+        try {
+            usuarioJpa.destroy(id);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(ControladoraPersistencia.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
