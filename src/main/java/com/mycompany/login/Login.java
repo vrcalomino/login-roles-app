e4be1a4004b702fe8a66417df9c7a19965665176
vrcalomino <vrcalomino@gmail.com>
2023-11-22 12:05:23 -0300

    initialize login screen

--- src/main/java/com/mycompany/login/Login.java
+++ src/main/java/com/mycompany/login/Login.java
@@ -1,16 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.login;
 
-/**
- *
- * @author vcalo
- */
+import com.mycompany.login.igu.IniciarSesion;
+
 public class Login {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        IniciarSesion pantalla = new IniciarSesion();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
     }
 }
