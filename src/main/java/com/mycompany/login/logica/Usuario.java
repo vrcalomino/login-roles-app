4f18a6d2d7d6da5aa2f061178ad69c3f9765a7e2
vrcalomino <vrcalomino@gmail.com>
2023-11-22 11:46:56 -0300

    feat: usuario class

---
+++ src/main/java/com/mycompany/login/logica/Usuario.java
@@ -0,0 +1,47 @@
+package com.mycompany.login.logica;
+
+public class Usuario {
+    
+    private int id;
+    private String nombreUsuario;
+    private String contraseña;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id, String nombreUsuario, String contraseña) {
+        this.id = id;
+        this.nombreUsuario = nombreUsuario;
+        this.contraseña = contraseña;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "id=" + id + ", nombreUsuario=" + nombreUsuario + '}';
+    }
+    
+}
