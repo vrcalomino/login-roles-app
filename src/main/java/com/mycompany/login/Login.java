199fcc4287c1057e64ce8c4582c6532377e03f36
vrcalomino <vrcalomino@gmail.com>
2023-11-22 11:31:11 -0300

    layer model setup

---
+++ src/main/java/com/mycompany/login/Login.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.login;
+
+/**
+ *
+ * @author vcalo
+ */
+public class Login {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
